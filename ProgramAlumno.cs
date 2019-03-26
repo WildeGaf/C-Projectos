92e8c4694e7af992aace33210946a514d47142e5
WildeGaf <40776504+WildeGaf@users.noreply.github.com>
2019-03-25 22:11:30 -0300

    Add files via upload

---
+++ ProgramAlumno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Flores","Gonzalo");
+            Alumno alumno2 = new Alumno("asd", "qwe");
+            Alumno alumno3 = new Alumno("zxc", "vbn");
+            alumno1.Estudiar(5,8);
+            alumno2.Estudiar(2,6);
+            alumno3.Estudiar(10,7);
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+            Alumno.colegio = "UTN";
+            // alumno1.apellido = "Perez";
+            Console.WriteLine(alumno1.Mostrar());
+            Console.WriteLine(alumno2.Mostrar());
+            Console.WriteLine(alumno3.Mostrar());
+            Console.ReadKey();
+        }
+    }
+
+}
+
