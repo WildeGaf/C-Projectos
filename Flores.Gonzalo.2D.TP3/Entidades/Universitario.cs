e1357be2b5386c175b851920c92e337cf6905599
alumno <alumno@utnfra.com.ar>
2019-06-04 21:55:50 -0300

    TP 3

---
+++ Flores.Gonzalo.2D.TP3/Entidades/Universitario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Universitario : Persona
+    {
+        private int legajo;
+
+        public Universitario()
+        {
+
+        }
+
+        public Universitario(int legajo,string nombre, string apellido, string dni,ENacionalidad nacionalidad) : base(nombre, apellido, dni, nacionalidad)
+        {
+            this.legajo = legajo;
+        }
+
+
+    }
+
+
+
+}
